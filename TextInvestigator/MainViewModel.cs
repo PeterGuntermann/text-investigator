7b70256a2bad25c4d5db4cdfa6977b1b07b268bd
Peter Guntermann <peter.guntermann@kardex.com>
2020-11-13 10:04:35 +0100

    Add OxyPlot

---
+++ TextInvestigator/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TextInvestigator
+{
+    using System;
+    using OxyPlot;
+    using OxyPlot.Series;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            TextToInvestigate = "Insert text here that needs to be investigated.";
+            MyModel = new PlotModel {Title = "Text Investigator"};
+            MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public string TextToInvestigate { get; set; }
+        public PlotModel MyModel { get; private set; }
+    }
+}
